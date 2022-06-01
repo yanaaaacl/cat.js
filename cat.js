aa35f3556dc9df1637d9451387cf9e32aac8da92
yanaaaacl <a9506862137@yandex.ru>
2022-06-01 10:21:51 +0300

    my first programm in js

---
+++ cat.js
@@ -0,0 +1,7 @@
+// Рисуем столько котиков, сколько захотим!
+var drawCats = function (howManyTimes) {
+    for (var i=0; i < howManyTimes; i++) {
+        console.log(i + "=^.^=");
+    }
+};
+drawCats(10); // Вместо 10 тут может быть любое число
